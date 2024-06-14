a9200b485932d89f6b9a18a8439a31ef9607ea42
Mohamed Amer <139323704+atmohamedamer@users.noreply.github.com>
2024-06-14 05:01:02 -0700

    Upgrade to vite

---
+++ subjektify.config.ts
@@ -0,0 +1,9 @@
+import { SubjektifyConfig } from 'subjektify';
+
+const config: SubjektifyConfig = {
+    namespace: "subjektify.blueprints",
+    version: "0.0.1",
+    license: "MIT"
+}
+
+export default config;
